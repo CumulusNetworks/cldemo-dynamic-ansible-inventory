1ee6f6797fb0bacbaf0f67ae9bac3b79ff0888e2
Pete Lumbis <plumbis@cumulusnetworks.com>
2016-11-11 18:19:19 -0500

    modified structure for ansible

--- build_database.py
+++ build_database.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python
 
+# Requires pip install redis
 import redis
 conn = redis.Redis('localhost')
+
+'''
+The easiest way to manage the ansible info within redis
+is to turn the vars yaml file into a python dict.
+
+But redis is funky about how it handles complex datastructures (like nested dicts).
+The easy solution is treat it all like JSON.
+
+Each device has a string of settings.
+
+That string is loaded into redis can then can be modified through python.
+
+Since this is a demo around reading inventory and settings from an external source,
+no effort was put into making adding/modifying nodes easier.
+'''
+inventory = '{ \
+    "network": { \
+        "hosts": ["spine01", "leaf01"], \
+        "vars": { \
+            "ansible_user": "cumulus", \
+            "ansible_ssh_pass": "CumulusLinux!", \
+            "ansible_become_pass": "CumulusLinux!" \
+        } \
+    } \
+}'
+
 
 leaf01 = '{ \
     "settings": {"ansible_user": "cumulus", "ansible_ssh_pass": "CumulusLinux!", "ansible_become_pass": "CumulusLinux!"} \
@@ -14,3 +41,4 @@ spine01 = '{ \
 
 conn.set("leaf01", leaf01)
 conn.set("spine01", spine01)
+conn.set("inventory", inventory)
