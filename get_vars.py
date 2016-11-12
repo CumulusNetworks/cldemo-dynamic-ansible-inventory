9420172e55797dd5736037ec3ba813a84e3f1ba3
Pete Lumbis <plumbis@cumulusnetworks.com>
2016-11-12 13:18:41 -0500

    adding list arg

--- get_vars.py
+++ get_vars.py
@@ -10,14 +10,19 @@ conn = redis.Redis('localhost')
 def get_inventory():
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', action='store')
+    parser.add_argument('--list', action='store')
     args = parser.parse_args()
 
     if args.host:
+        return json.loads(conn.get("inventory"))
+    elif args.list:
         return json.loads(conn.get("inventory"))
     else:
         return {'_meta': {'hostvars': {}}}
 
 
+# get_var.py --host leaf01
+# {u'network': {u'hosts': [u'spine01', u'leaf01'], u'vars': {u'ansible_ssh_pass': u'CumulusLinux!', u'ansible_user': u'cumulus', u'ansible_become_pass': u'CumulusLinux!'}}}
 get_inventory()
 
 #redis_string = conn.get("leaf02")
